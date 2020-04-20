dad3ce800d8e7336b02e2980e833bcb15345a462
Christina McMahon <christinamcmahon@Christinas-MacBook-Pro.local>
2020-04-20 11:53:37 -0700

    complete regex parser

---
+++ RegexParser.js
@@ -0,0 +1,36 @@
+function isMatch(text, pattern) {
+    // edge cases: empty text or pattern, pattern is just "*"
+    // if text is empty but pattern is not empty, return false
+    // if both are empty, return true
+    // if pattern is empty, return false (?) 
+    if (pattern.length < text.length) {
+        return false
+    }
+    // track indexes of text and pattern 
+    let textIndex = 0
+    // if pattern[i] is a ".", then skip an index in text and move on
+    for (let i = 0; i < pattern.length; i++) {
+        if (pattern[i] === text[textIndex] || pattern[i] === ".") {
+            textIndex++
+        } else if (pattern[i] === "*") {
+            while (text[textIndex] === pattern[i - 1]) {
+                textIndex++
+            }
+        } else if (pattern[i + 1] === "*") {
+            break
+        } else if (pattern[i] !== text[textIndex]) {
+            return false
+        }
+    }
+    return true
+    // if pattern[i] is a "*", then check pattern[i-1] and go through text until you reach something that does not match that
+    // if pattern[i] isn't one of the above, just compare with text[j]
+    // if you ever get through all of those without a match, return false
+    // if you reach the end of both, return true
+}
+
+  // examples:
+  // isMatch("aa", "a") => false
+  // isMatch("abc", "a.c") => true
+  // isMatch("abbb", "ab*") => true
+  // isMatch("acd", "ab*c.") => true
