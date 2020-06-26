6d204ae403eb21891f267c827c8fadce8e3c1262
Christina McMahon <christina.mcmahon1@gmail.com>
2020-06-26 15:59:33 -0700

    Pramp interview work - regex parser

--- RegexParser.js
+++ RegexParser.js
@@ -6,26 +6,56 @@ function isMatch(text, pattern) {
     // if text is empty but pattern is not empty, return false
     // if both are empty, return true
     // if pattern is empty, return false (?) 
-    // track indexes of text and pattern 
+    if (!pattern.includes(".") && !pattern.includes("*")) {
+        return text === pattern
+    }
     let textIndex = 0
-    // if pattern[i] is a ".", then skip an index in text and move on
     let len = pattern.length > text.length ? pattern.length : text.length
-    for (let i = 0; i < len; i++) {
-        if (pattern[i] === text[textIndex] || pattern[i] === ".") {
+    for (let patternIndex = 0; patternIndex <= len && textIndex < text.length; patternIndex++) {
+        if (pattern[patternIndex] === text[textIndex] || pattern[patternIndex] === ".") {
             textIndex++
-        } else if (pattern[i] === "*") {
-            while (text[textIndex] === pattern[i - 1]) {
+        } else if (pattern[patternIndex] === "*") {
+            while (text[textIndex] === pattern[patternIndex - 1]) {
                 textIndex++
             }
-        } else if (pattern[i + 1] === "*") {
+        } else if (pattern[patternIndex + 1] === "*") {
             break
-        } else if (pattern[i] !== text[textIndex]) {
+        } else if (pattern[patternIndex] !== text[textIndex]) {
             return false
         }
     }
-    return true
+    return textIndex >= text.length - 1
+    // track indexes of text and pattern 
+    // if pattern[i] is a ".", then skip an index in text and move on
     // if pattern[i] is a "*", then check pattern[i-1] and go through text until you reach something that does not match that
     // if pattern[i] isn't one of the above, just compare with text[j]
     // if you ever get through all of those without a match, return false
     // if you reach the end of both, return true
 }
+
+// function isMatch(text, pattern) {
+//     if (!pattern.includes(".") && !pattern.includes("*")) {
+//         return text === pattern
+//     }
+//     let textIndex = 0
+//     let len = pattern.length > text.length ? pattern.length : text.length
+//     for (let patternIndex = 0; patternIndex <= len && textIndex < text.length; patternIndex++) {
+//       console.log(`pattern[${patternIndex}]: ${pattern[patternIndex]} text[${textIndex}]: ${text[textIndex]}`)
+//         if (pattern[patternIndex] === text[textIndex] || pattern[patternIndex] === ".") {
+//             textIndex++
+//         } else if (pattern[patternIndex] === "*") {
+//             while (text[textIndex] === pattern[patternIndex - 1]) {
+//                 textIndex++
+//             }
+//         } else if (pattern[patternIndex + 1] === "*") {
+//             console.log("breaking")
+//             break
+//         } else if (pattern[patternIndex] !== text[textIndex]) {
+//             return false
+//         }
+//     }
+//     console.log("left loop")
+//     return textIndex >= text.length - 1
+// };
+
+// console.log(isMatch("acd", "ab*c."))
